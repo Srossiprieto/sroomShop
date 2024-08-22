31998b3691cf9b26a6a4959be4239f39b767c693
Santino <srossiprieto@gmail.com>
2024-08-22 16:41:58 -0300

    fix: routes-path

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home/home';
-import Error from './pages/Error/error';
-import About from './pages/About/about';
+import Home from './pages/home/home';
+import Error from './pages/error/error';
+import About from './pages/about/about';
 import { ThemeProvider } from './components/ThemeContext/ThemeContext';
 
 function App() {

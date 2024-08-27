ad0c0ed83d140f9b45ccc1019e3624e2f4b70c09
Santino <srossiprieto@gmail.com>
2024-08-27 16:14:26 -0300

    fix: Layout-color

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Layout from './Layout/Layout';
 import Home from './pages/home/home';
 import Error from './pages/error/error';
 import About from './pages/about/about';
+import { ThemeProvider } from './contexts/ThemeContext/ThemeContext';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Layout>
+      <ThemeProvider>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </Router>
   );
 }
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,19 +2,22 @@ import Footer from "../components/Footer/Footer"
 import Navbar from "../components/Navbar/Navbar"
 import styles from "./Layout.module.css"
 
+
 function Layout({children}) {
   return (
-    <div className={styles.container}>
-        <header>
-            <Navbar/>
-        </header>
-        <main>
-            {children}
-        </main>
-        <footer>
-            <Footer/>
-        </footer>
-    </div>
+    <>
+        <div className={styles.container}>
+            <header>
+                <Navbar/>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <Footer/>
+            </footer>
+        </div>
+    </>
   )
 }
 

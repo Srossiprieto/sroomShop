fb35bf21eb6f2ea65810c4cb462e9e8212a0d68d
Santino <srossiprieto@gmail.com>
2024-09-04 19:13:44 -0300

    feat: home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,6 @@
 import styles from './home.module.css';
-import About from '../about/about';
 import Button from '../../components/Ui/Button/Button';
 import RightSvg from '../../assets/Icons/right';
-
 
 function Home() {
   return (
@@ -10,21 +8,19 @@ function Home() {
     <div className={styles.containerGeneral}>
       <div className={styles.containerHome}>
         <div className= {styles.containerFlex}>
-          <h1>Home Page</h1>
+          <div className= {styles.containerText}>
+            <h1>Home Page</h1>
             <Button>
-              <RightSvg />
-              Click me
-            </Button>
-        </div>
-        <div className= {styles.containerFlex}>
-          <h1>Home Page</h1>
-            <Button>
-              <RightSvg />
-              Click me
-            </Button>
+                <RightSvg />
+                Click me
+              </Button>
+          </div>
+          <div className= {styles.containerImg}>
+            <img src='' alt="apple" />
+          </div>
+           
         </div>
       </div>
-        <About />
     </div>
     </>
   );
